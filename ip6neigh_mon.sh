24d1c2f3ef8f98abf12f3dacb922e341c9a13a0f
André <andredbl@hotmail.com>
2016-12-22 00:04:39 -0200

    New features
    - Auto probes for addresses with the same interface identifier in
    different scopes.
    - Speed up detection at script startup by pinging all hosts multicast
    address.

--- ip6neigh_mon.sh
+++ ip6neigh_mon.sh
@@ -30,6 +30,7 @@ config_get gua_label config gua_label
 config_get EUI64_LABEL config eui64_label SLAAC
 config_get TMP_LABEL config tmp_label TMP
 config_get PROBE_EUI64 config probe_eui64 1
+config_get_bool PROBE_IID config probe_iid 1
 config_get UNKNOWN config unknown "Unknown-"
 config_get_bool LOG config log 0
 
@@ -84,49 +85,69 @@ is_eui64() {
 
 #Generates EUI-64 interface identifier based on MAC address
 gen_eui64() {
-		local mac=$(echo "$2" | tr -d ':')
-		local iid1="${mac:0:4}"
-		local iid2="${mac:4:2}ff:fe${mac:6:2}:${mac:8:4}"
+	local mac=$(echo "$2" | tr -d ':')
+	local iid1="${mac:0:4}"
+	local iid2="${mac:4:2}ff:fe${mac:6:2}:${mac:8:4}"
 
-		#Flip U/L bit
-		iid1=$(printf %x $((0x${iid1} ^ 0x0200)))
+	#Flip U/L bit
+	iid1=$(printf %x $((0x${iid1} ^ 0x0200)))
 		
-		eval "$1=${iid1}:${iid2}"
-		return 0
+	eval "$1=${iid1}:${iid2}"
+	return 0
 }
 
-#Probe addresses with EUI-64 interface identifier based on the supplied MAC.
-probe_eui64() {
-	local mac="$1"
-	local scope="$2"
-	
-	#Generates EUI-64 interface identifier
-	local iid
-	gen_eui64 iid "$mac"
-	
+#Probe addresses related to the supplied base address and MAC.
+probe_addresses() {
+	local baseaddr="$1"
+	local mac="$2"
+	local scope="$3"
+
 	#Select addresses for probing
 	local list=""
-	if [ "$PROBE_EUI64" = "1" ] && [ "$scope" != 0 ]; then
-		list="fe80::${iid} "
+
+	#Check if is configured for probing addresses with the same IID
+	local base_iid=""
+	if [ "$PROBE_IID" -gt 0 ]; then
+		#Gets the interface identifier from the base address
+		base_iid=$(echo "$baseaddr" | grep -o -m 1 -E "[^:]{1,4}:[^:]{1,4}:[^:]{1,4}:[^:]{1,4}$")
+
+		#Probe same IID for different scopes.
+		if [ "$scope" != 0 ]; then list="${list}fe80::${base_iid} "; fi
+		if [ "$scope" != 1 ]; then list="${list}${ula_prefix}:${base_iid} "; fi
+		if [ "$scope" != 2 ]; then list="${list}${gua_prefix}:${base_iid} "; fi
 	fi
-	if [ "$PROBE_EUI64" = "1" ] || [ "$scope" = 1 ]; then
-		list="${list}${ula_prefix}:${iid} "
-	fi
-	if [ "$PROBE_EUI64" = "1" ] || [ "$scope" = 2 ]; then
-		list="${list}${pub_prefix}:${iid}"
+
+	#Check if is configured for probing MAC-based addresses
+	if [ "$PROBE_EUI64" -gt 0 ]; then
+		#Generates EUI-64 interface identifier
+		local eui64_iid
+		gen_eui64 eui64_iid "$mac"
+
+		#Only add to list if EUI-64 IID is different from the one that has been just added.
+		if [ "$eui64_iid" != "$base_iid" ]; then
+			if [ "$PROBE_EUI64" = "1" ] && [ "$scope" != 0 ]; then list="${list}fe80::${eui64_iid} "; fi
+			if [ "$PROBE_EUI64" = "1" ] || [ "$scope" = 1 ]; then list="${list}${ula_prefix}:${eui64_iid} "; fi
+			if [ "$PROBE_EUI64" = "1" ] || [ "$scope" = 2 ]; then list="${list}${gua_prefix}:${eui64_iid}"; fi
+		fi
 	fi
 	
 	#Exit if there is nothing to probe.
 	[ -n "$list" ] || return 0
 	
-	[ "$LOG" -gt 0 ] && logger -t ip6neigh "Probing addresses for MAC ${mac}: ${list}"
+	[ "$LOG" -gt 0 ] && logger -t ip6neigh "Probing addresses: ${list}"
 	
 	#Ping each address once
 	local addr
 	IFS=' '
 	for addr in $list; do
-		[ -n "$addr" ] && ping6 -W 1 -c 1 "$addr" >/dev/null 2>/dev/null
+		if [ -n "$addr" ]; then
+			#Skip addresses that already exist in some hosts file.
+			if ! grep -q "^$addr " /tmp/hosts/* ; then
+		 		ping6 -q -W 1 -c 1 -s 1 -I "$LAN_DEV" "$addr" >/dev/null 2>/dev/null
+			fi
+		fi
 	done
+
 	return 0
 }
 
@@ -175,8 +196,8 @@ process() {
 				#Is link-local. Append corresponding label.
 				suffix="${LL_LABEL}"
 				
-				#Probe for MAC-based addresses with the same prefix.
-				[ "$PROBE" != "0" ] && probe_eui64 "$mac" 0	
+				#Probe addresses related to this one.
+				probe_addresses "$addr" "$mac" 0
 			elif [ "${addr:0:2}" = "fd" ]; then
 				#Is ULA. Append corresponding label.
 				suffix="${ULA_LABEL}"
@@ -191,10 +212,10 @@ process() {
 				else
 					#Interface identifier is not EUI-64. #Adds temporary address label.
 					suffix="${TMP_LABEL}${suffix}"
-					
-					#Probe for MAC-based addresses with the same prefix.
-					[ "$PROBE" != "0" ] && probe_eui64 "$mac" 1
 				fi
+
+				#Probe addresses related to this one.
+				probe_addresses "$addr" "$mac" 1
 			else
 				#The address is globally unique. Append corresponding label.
 				suffix="${GUA_LABEL}"
@@ -208,11 +229,11 @@ process() {
 					fi
 				else
 					#Interface identifier is not EUI-64. #Adds temporary address label.
-					suffix="${TMP_LABEL}${suffix}"
-					
-					#Probe for MAC-based addresses with the same prefix.
-					[ "$PROBE" != "0" ] && probe_eui64 "$mac" 2
+					suffix="${TMP_LABEL}${suffix}"					
 				fi 
+
+				#Probe addresses related to this one.
+				probe_addresses "$addr" "$mac" 2
 			fi
 
 			#Cat strings to get FQDN
@@ -233,37 +254,54 @@ config_host() {
 
 	config_get name "$1" name
 	config_get mac "$1" mac
-	config_get slaac "$1" slaac
+	config_get slaac "$1" slaac "0"
 
 	#Ignore entry if required options are absent.
 	if [ -z "$name" ] || [ -z "$mac" ] || [ -z "$slaac" ]; then
 		return 0
 	fi
 
-	local iid
-
-	#Check if slaac flag is set
-	if [ "$slaac" = "1" ]; then
-		#Generates EUI-64 interface identifier based on MAC entry
-		gen_eui64 iid "$mac"
-	elif [ "$slaac" != "0" ]; then
-		#slaac option carries a custom interface identifier. Just copy it.
-		iid="$slaac"
-	fi
+	#Do nothing if slaac option is disabled
+	[ "$slaac" != "0" ] || return 0
 	
-	[ "$LOG" -gt 0 ] && logger -t ip6neigh "Generating predefined SLAAC addresses for $name with IID $iid"
+	#slaac option is enabled. Check if it contains a custom IID.
+	local iid
+	if [ "$slaac" != "1" ]; then
+		#Use custom IID
+		iid="$slaac"
+	else
+		#Generates EUI-64 interface identifier based on MAC
+		gen_eui64 iid "$mac"
+	fi
 
-	#Creates hosts file entries with link-local, ULA and GUA prefixes with the same interface identifier.
-	echo "fe80::${iid} ${name}${LL_LABEL}.${DOMAIN}" >> /tmp/hosts/ip6neigh
-	[ -n "$ula_prefix" ] && echo "${ula_prefix}:${iid} ${name}${ULA_LABEL}.${DOMAIN}" >> /tmp/hosts/ip6neigh
-	[ -n "$pub_prefix" ] && echo "${pub_prefix}:${iid} ${name}${GUA_LABEL}.${DOMAIN}" >> /tmp/hosts/ip6neigh
+	#Load custom interface identifiers for each scope of address.
+	#Uses EUI-64 when not specified.
+	local ll_iid
+	local ula_iid
+	local gua_iid
+	config_get ll_iid "$1" ll_iid "$iid"
+	config_get ula_iid "$1" ula_iid "$iid"
+	config_get gua_iid "$1" gua_iid "$iid"
+	
+	[ "$LOG" -gt 0 ] && logger -t ip6neigh "Generating predefined SLAAC addresses for $name"
+
+	#Creates hosts file entries with link-local, ULA and GUA prefixes with corresponding IIDs.
+	if [ "$ll_iid" != "0" ]; then
+		echo "fe80::${ll_iid} ${name}${LL_LABEL}.${DOMAIN}" >> /tmp/hosts/ip6neigh
+	fi
+	if [ -n "$ula_prefix" ] && [ "$ula_iid" != "0" ]; then
+		echo "${ula_prefix}:${ula_iid} ${name}${ULA_LABEL}.${DOMAIN}" >> /tmp/hosts/ip6neigh
+	fi
+	if [ -n "$gua_prefix" ] && [ "$gua_iid" != "0" ]; then
+		echo "${gua_prefix}:${gua_iid} ${name}${GUA_LABEL}.${DOMAIN}" >> /tmp/hosts/ip6neigh
+	fi
 }
 
 #Finds ULA and global prefixes on LAN interface.
 ula_cidr=$(ip -6 addr show "$LAN_DEV" scope global 2>/dev/null | grep "inet6 fd" | grep -m 1 -v "dynamic" | awk '{print $2}')
 pub_cidr=$(ip -6 addr show "$LAN_DEV" scope global dynamic 2>/dev/null | grep -m 1 -E "inet6 ([^fd])" | awk '{print $2}')
 ula_prefix=$(echo "$ula_cidr" | cut -d ":" -f1-4)
-pub_prefix=$(echo "$pub_cidr" | cut -d ":" -f1-4)
+gua_prefix=$(echo "$pub_cidr" | cut -d ":" -f1-4)
 
 #Decides if the GUAs should get a label based in config file and the presence of ULAs
 if [ -n "$gua_label" ]; then
@@ -300,8 +338,9 @@ echo -e "\n#Detected IPv6 neighbors" >> /tmp/hosts/ip6neigh
 #Send signal to dnsmasq to reload hosts files.
 killall -1 dnsmasq
 
-#Flushes the neighbors cache to speedup detection.
+#Flushes the neighbors cache and pings "all nodes" multicast address to speedup detection.
 ip -6 neigh flush dev "$LAN_DEV"
+ping6 -q -W 1 -c 3 -s 1 -I "$LAN_DEV" ff02::1 >/dev/null 2>/dev/null
 
 #Infinite loop. Keeps monitoring changes in IPv6 neighbor's reachability status and call process() routine.
 ip -6 monitor neigh dev "$LAN_DEV" |
