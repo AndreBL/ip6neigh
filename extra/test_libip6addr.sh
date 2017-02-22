8576a17df13587ab94344b77b40974896774d00c
Craig Miller <cvmiller@gmail.com>
2017-02-22 15:19:54 -0800

    version 1.1.1 updated to run on host or router, added case statement  to test indivdual api calls with '-f'

--- extra/test_libip6addr.sh
+++ extra/test_libip6addr.sh
@@ -1,4 +1,4 @@
-!/bin/sh
+#!/bin/sh
 ##################################################################################
 #
 #  Copyright (C) 2017 Craig Miller
@@ -15,13 +15,19 @@
 #	by Craig Miller		20 Feb 2017
 
 
-source ../lib/ip6addr_functions.sh
+if [ -e ../lib/ip6addr_functions.sh ]; then
+	# used for testing off router
+	. ../lib/ip6addr_functions.sh
+else
+	# testing on the router
+	. /usr/lib/ip6neigh/ip6addr_functions.sh
+fi
 
-#VERSION 1.1.0
+#VERSION 1.1.1
 
 test() {
 
-	if [ $(compress_addr $(expand_addr $1)) == $1 ]; then
+	if [ "$(compress_addr $(expand_addr $1))" == "$1" ]; then
 		echo "$1 -- OK"
 	else
 		echo "$1 -- FAILED"
@@ -29,16 +35,28 @@ test() {
 
 }
 
-# self test - call with '-t' parameter
-if [ "$1" == "-t" ]; then
-	# add address examples to test
-	test fd11::1d70:cf84:18ef:d056
-	test 2a01::1
-	test fe80::f203:8cff:fe3f:f041
-	test 2001:db8:123::5
-	test 2001:0db8:0123::05
-	test 2001:470:ebbd:0:f203:8cff:fe3f:f041
-	# special cases
-	test ::1
-	test fd32:197d:3022:1101::
-fi
+
+case "$1" in
+	# self test - call with '-t' parameter
+	'-t')
+
+		# add address examples to test
+		test fd11::1d70:cf84:18ef:d056
+		test 2a01::1
+		test fe80::f203:8cff:fe3f:f041
+		test 2001:db8:123::5
+		test 2001:0db8:0123::05
+		test 2001:470:ebbd:0:f203:8cff:fe3f:f041
+		# special cases
+		test ::1
+		test fd32:197d:3022:1101::
+	;;
+	
+	# call a specific function with arguments - Example: -f expand_addr ::1
+	'-f')
+		shift 1
+		eval "$*"
+		expand_addr ::2
+	;;
+esac
+
